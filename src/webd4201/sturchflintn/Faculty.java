ce6360888cc6f99315a9fa5b612b60500c51637c
Nick Sturch-Flint <nickdoesreviews@gmail.com>
2021-01-08 12:23:05 -0500

    Added CollegeInterface and User class

---
+++ src/webd4201/sturchflintn/Faculty.java
@@ -0,0 +1,20 @@
+package webd4201.sturchflintn;
+
+/**
+ * @author Nick Sturch-Flint - 100303769
+ * @author Nick Sturch-Flint
+ * @version 1.0 (Janaury 8th, 2021)
+ */
+public class Faculty
+{
+
+    //CONSTANTS
+    public static final String DEFAULT_SCHOOL_CODE = "SET";
+    public static final String DEFAULT_SCHOOL_DESCRIPTION = "School of Engineering & Technology";
+    public static final String DEFULAT_OFFICE = "H-140";
+    public static final int DEFAULT_PHONE_EXTENSION = 1234;
+
+    //VARIABLES
+
+
+}
